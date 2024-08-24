bb997ba983a6c030016cce7bfb4e886352551898
Asadbek-Sotvoldiyev <blogasadbek@gmail.com>
2024-08-24 08:51:29 +0500

    sdfsdf

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
-from django.dispatch import receiver
 from .telegram_auth import auth_telegram
 import threading
 
