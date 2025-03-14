e50ca5a0a4f5d72d5c109c8ff144f2d591f693b0
Asadbek Sotvoldiyev <blogasadbek@gmail.com>
2025-03-14 15:52:48 +0500

    sdf

--- config/settings.py
+++ config/settings.py
@@ -131,10 +131,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000", 
-    "http://127.0.0.1:8000",  
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS').split(',')
 
 
 # Database
